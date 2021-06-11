b19c5900407dc7966889847d2dccfc28bbc9a1d3
sernamar <sernamar@protonmail.com>
2021-06-11 18:46:46 +0200

    Add a function to generate normal random numbers

--- src/dcf.py
+++ src/dcf.py
@@ -1,3 +1,6 @@
+from numpy.random import default_rng
+
+
 def compute_dcf(cash_flow_values, discount_rate):
     dcf = 0
     for year, cash_flow in enumerate(cash_flow_values):
@@ -5,8 +8,15 @@ def compute_dcf(cash_flow_values, discount_rate):
     return dcf
 
 
+def get_random_numbers(mean=0, standard_deviation=1, size=None):
+    "Generate one or more normal random numbers."
+    rng = default_rng()
+    return rng.normal(mean, standard_deviation, size)
+
+
 if __name__ == '__main__':
     cash_flow_values = [1000000, 1000000, 4000000, 4000000, 6000000]
     discount_rate = .05
     dcf = compute_dcf(cash_flow_values, discount_rate)
     print(f"Discounted Cash Flow = {dcf}")
+    print("Standard random numbers =", get_random_numbers(0, 1, 5))
--- test/test_dcf.py
+++ test/test_dcf.py
@@ -1,4 +1,4 @@
-from src.dcf import compute_dcf
+from src.dcf import compute_dcf, get_random_numbers
 
 
 def test_dcf():
@@ -6,3 +6,10 @@ def test_dcf():
     discount_rate = .05
     expected = 13306728
     assert expected == round(compute_dcf(cash_flow_values, discount_rate))
+
+
+def test_get_random_numbers():
+    # check that we get the right number of elements
+    assert 0 == len(get_random_numbers(size=0))
+    assert 1 == len(get_random_numbers(size=1))
+    assert 4 == len(get_random_numbers(size=4))
