7d8755c94549fc290d4906f79fa79dd8ab85123f
sernamar <sernamar@protonmail.com>
2021-06-12 07:52:55 +0200

    Check the order of some ASSERT expressions

--- test/test_dcf.py
+++ test/test_dcf.py
@@ -6,14 +6,14 @@ def test_compute_dcf():
     cash_flow_values = [1000000, 1000000, 4000000, 4000000, 6000000]
     discount_rate = .05
     expected = 13306728
-    assert expected == round(compute_dcf(cash_flow_values, discount_rate))
+    assert round(compute_dcf(cash_flow_values, discount_rate)) == expected
 
 
 def test_get_random_numbers():
     # check that we get the right number of elements
-    assert 0 == len(get_random_numbers(size=0))
-    assert 1 == len(get_random_numbers(size=1))
-    assert 4 == len(get_random_numbers(size=4))
+    assert len(get_random_numbers(size=0)) == 0
+    assert len(get_random_numbers(size=1)) == 1
+    assert len(get_random_numbers(size=4)) == 4
 
 
 def test_simulate_cash_flow_values():
